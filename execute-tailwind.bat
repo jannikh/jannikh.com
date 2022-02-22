119b57b8eb1afad92fb1eaf1972d4fbb7229eced
Jannik Hofmann <jannik.hofmann@student.uni-tuebingen.de>
2022-02-22 12:49:05 +0100

    minor adjustments - now minifying css code in tailwind - corrected typo in word "Model(l)ing" - adjusted linked email address to be consistent with link text

--- execute-tailwind.bat
+++ execute-tailwind.bat
@@ -1,2 +1,2 @@
-npx tailwindcss -i ./src/styles.css -o ./build/styles.css --watch
+npx tailwindcss -i ./src/styles.css -o ./build/styles.css --minify --watch
 msg "%username%" TAILWIND BUILD WATCHER STOPPED!
